02d532f9b398ce681acb738d13746f24af26f702
EnesBey <enes@MacBookAir.home>
2026-06-03 00:59:53 +0300

    UserLoginSystem Project

---
+++ Beginner/12_UserLoginSystem.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    // Predefined username and password
+    string correctUsername = "admin";
+    string correctPassword = "admin";
+
+    string username;
+    string password;
+
+    int attempts = 3; // Number of allowed login attempts
+
+    // Allow user 3 attempts to login
+    while (attempts > 0) {
+
+        cout << "\n===== LOGIN SYSTEM =====\n";
+
+        // Get username from user
+        cout << "Enter username: ";
+        cin >> username;
+
+        // Get password from user
+        cout << "Enter password: ";
+        cin >> password;
+
+        // Check credentials
+        if (username == correctUsername && password == correctPassword) {
+            cout << "Login successful! Welcome " << username << endl;
+            break;
+        }
+        else {
+            attempts--; // Reduce remaining attempts
+            cout << "Wrong username or password!" << endl;
+            cout << "Remaining attempts: " << attempts << endl;
+        }
+
+        // If attempts are over
+        if (attempts == 0) {
+            cout << "Account locked!" << endl;
+        }
+    }
+
+    return 0;
+}
